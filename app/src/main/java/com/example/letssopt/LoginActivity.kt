67f0e6960ecb8bb9bee2699f477c22c4d4d73df5
chldmswll <cuicui633@naver.com>
2026-04-08 22:55:14 +0900

    upload

---
+++ app/src/main/java/com/example/letssopt/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.example.letssopt
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LongState
+import androidx.compose.runtime.Recomposer
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.example.letssopt.ui.theme.LETSSOPTTheme
+
+
+class LoginActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
+
+@Composable
+fun Login(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 15.dp)
+    ){
+        Text(text = "로그인 성공 !!")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    LETSSOPTTheme {
+        Login("Android")
+    }
+}
--- app/src/main/java/com/example/letssopt/SignUpActivity.kt
+++ app/src/main/java/com/example/letssopt/SignUpActivity.kt
@@ -2,7 +2,6 @@ package com.example.letssopt
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,26 +16,34 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LongState
+import androidx.compose.runtime.Recomposer
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import com.example.letssopt.ui.theme.LETSSOPTTheme
 
-class MainActivity : ComponentActivity() {
+
+class SignUpActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LETSSOPTTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Week1",
+                    Signup(
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -46,75 +53,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Signup(name: String, modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    val intent = Intent(context, SignUpActivity::class.java)
+    val intent = Intent(context, MainActivity::class.java)
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(horizontal = 15.dp))
     {
-
         Text(
             text = "Watcha",
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+            fontWeight = FontWeight.Bold,
             modifier = modifier
         )
         Text(
-            text = "이메일로 로그인",
+            text = "회원가입",
             modifier = modifier
         )
         Text(
             text = "이메일",
             modifier = modifier
         )
-        var emailinput by remember {mutableStateOf("")}
+
+        var email by remember {mutableStateOf("")}
         TextField(
-            value = emailinput,
-            onValueChange = { emailinput = it },
+            value = email,
+            onValueChange = { email = it },
             modifier = Modifier
                 .fillMaxWidth(),
-            label = { Text(text = "이메일 주소를 입력하세요") }
+            label = { Text("이메일 주소를 입력하세요") }
         )
+
         Text(
             text = "비밀번호",
             modifier = modifier
         )
-        var pwinput by remember {mutableStateOf("")}
+        var pw by remember {mutableStateOf("")}
         TextField(
-            value = pwinput,
-            onValueChange = { pwinput =it },
+            value = pw,
+            onValueChange = { pw = it },
             modifier = Modifier
                 .fillMaxWidth(),
             label = { Text(text = "비밀번호를 입력하세요") }
         )
         Text(
-            text = "아직 계정이 없으신가요?",
+            text = "비밀번호 확인",
             modifier = modifier
+        )
+        TextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier
+                .fillMaxWidth(),
+            label = { Text(text = "비밀번호를 다시 입력하세요") }
         )
         Button(
             onClick = {
+                intent.putExtra("email",email)
+                intent.putExtra("password",pw)
                 context.startActivity(intent)
-
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        ) {
-            Text("회원가입", color = Color.White)
-        }
-        Button(
-            onClick = {
-                val email = intent.getStringExtra("email")
-                val pw = intent.getStringExtra("password")
-                if (emailinput == email && pwinput == pw){
-                    val intent3 = Intent(context, LoginActivity::class.java)
-                    context.startActivity(intent3)
-                }
-                else {
-                    // putextra,getextra 확인용
-                    Toast.makeText(context, "email: $email emailinput: $emailinput", Toast.LENGTH_SHORT).show()
-                    Toast.makeText(context, "pw: $pw pwinput: $pwinput", Toast.LENGTH_SHORT).show()
-                }
             },
             modifier = Modifier
                 .fillMaxWidth()
@@ -122,17 +121,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
             shape = RoundedCornerShape(8.dp)
         ) {
-            Text("로그인", color = Color.White)
+            Text("회원가입", color = Color.White)
         }
-
     }
 
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SignupPreview() {
     LETSSOPTTheme {
-        Greeting("Android")
-    }
-}
+        Signup("Android")
+    }}
