b2ecf5b148c4ad38736d798da6179422045dec23
chldmswll <cuicui633@naver.com>
2026-05-11 15:56:35 +0900

    Week4 코드리뷰 4차 반영

--- app/src/main/java/com/example/letssopt/ApiService.kt
+++ app/src/main/java/com/example/letssopt/ApiService.kt
@@ -3,13 +3,14 @@ package com.example.letssopt
 import com.example.letssopt.auth.BaseResponseDto
 import com.example.letssopt.auth.login.LoginRequestDto
 import com.example.letssopt.auth.signup.SignUpRequestDto
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("api/v1/auth/signup")
-    suspend fun signUp(@Body request: SignUpRequestDto): BaseResponseDto<SignUpRequestDto>
+    suspend fun signUp(@Body request: SignUpRequestDto): Response<BaseResponseDto<SignUpRequestDto>>
 
     @POST("api/v1/auth/signin")
-    suspend fun logIn(@Body request: LoginRequestDto): BaseResponseDto<Unit>
+    suspend fun logIn(@Body request: LoginRequestDto): Response<BaseResponseDto<Unit>>
 }
